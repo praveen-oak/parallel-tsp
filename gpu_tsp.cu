ffc7b7a9d6e2042e1728cf5c9624f60b26726904
Praveen Oak <praveenoak@10-16-227-54.dynapool.wireless.nyu.edu>
2019-12-15 19:29:01 -0500

    adding comments

--- gpu_tsp.cu
+++ gpu_tsp.cu
@@ -3,15 +3,16 @@
 #include <assert.h>
 #include <pthread.h>
 
-//comment
-#define index(i,j,cities)((i*cities)+j)
 #define THREADS_X 16
 #define THREADS_Y 16
 
 #define BLOCKS_X 16
 #define BLOCKS_Y 16
 
+
 int devices = 0;
+
+//structure to send to each pthread
 struct arg_struct {
     float *cpu_distance;
     float *gpu_distance;
@@ -20,7 +21,10 @@ struct arg_struct {
     int device_index;
 };
 
+//function to be called by each thread
 void *tsp(void *arguements);
+
+
 int main(int argc, char * argv[])
 {
 	if(argc != 4){
@@ -32,18 +36,27 @@ int main(int argc, char * argv[])
 	FILE *fp=fopen(argv[2], "r");
 	FILE *fp_optimum=fopen(argv[3], "r");
 
+	//read the data from the file
 	float *distance = (float *)malloc(distance_array_size);
 	unsigned int *tour = (unsigned int *)malloc((cities+1)*sizeof(unsigned int *));
 	read_files(fp, fp_optimum, distance, tour, cities);	
 	fclose(fp);
 	fclose(fp_optimum);
 
+	//create thread structures
 	struct arg_struct args[8];
-	pthread_t stream_threads[8];
+	pthread_t threads[8];
 
+	//get number of devices available
 	CUDA_CALL(cudaGetDeviceCount (&devices));
 	float *gpu_distance;
 
+	//use multiple devices only if problem size warrants it
+	if(cities < 200){
+		devices = 1;
+	}
+
+	//run a thread for each device
 	for(int i = 0; i < devices; i++){
 
 		CUDA_CALL(cudaSetDevice(i));
@@ -57,21 +70,27 @@ int main(int argc, char * argv[])
 		args[i].gpu_distance= gpu_distance;
 		args[i].return_pointer = (float *)malloc(sizeof(float));
 		args[i].device_index = i;
-		if (pthread_create(&stream_threads[i], NULL, &tsp, (void *)&args[i]) != 0) {
+		if (pthread_create(&threads[i], NULL, &tsp, (void *)&args[i]) != 0) {
         	printf("Error in creating threads, exiting program\n");
         	return -1;
     	}
     	
 	}
+
+	//wait for threads
 	for(int i = 0; i < devices; i++){
-		pthread_join(stream_threads[i], NULL);	
+		pthread_join(threads[i], NULL);	
 	}
+
+	//get global minimum based on value returned by each thread
 	float min_val = FLT_MAX;
 	for(int i = 0; i < devices; i++){
 		if(args[i].return_pointer[0] < min_val){
 			min_val = args[i].return_pointer[0];
 		}
 	}
+
+
 	printf("Global minimum value is %f\n",min_val);
 	free(distance);
 	free(tour);
@@ -79,7 +98,9 @@ int main(int argc, char * argv[])
 
 }
 
+//GPU kernel that runs 2-opt algo in parallel
 __global__ void two_opt(unsigned int *cycle, float *distance, unsigned int cities, float *min_val_array, unsigned int* min_index_array){
+	//shared array to store most negetive decrease reported by each thread
 	__shared__ float temp_min[THREADS_Y*THREADS_X];
 	__shared__ float temp_min_index[THREADS_Y*THREADS_X];
 	float min_val = FLT_MAX;
@@ -90,7 +111,7 @@ __global__ void two_opt(unsigned int *cycle, float *distance, unsigned int citie
 			temp_val = distance[cycle[i]*cities + cycle[j+1]]+distance[cycle[i-1]*cities + cycle[j]]-distance[cycle[j]*cities + cycle[j+1]]-distance[cycle[i-1]*cities + cycle[i]];
 			if(temp_val < min_val && i < j){
 				min_val = temp_val;
-				min_index = i*cities+j;
+				min_index = i*cities+j; //this is being done to save space, both i and j values can be stored and retrieved this way
 			}
 		}
 	}
@@ -104,6 +125,7 @@ __global__ void two_opt(unsigned int *cycle, float *distance, unsigned int citie
 	temp_min[tid] = min_val;
 	temp_min_index[tid] = min_index;
 
+	//now reduce the min array so that the index = 0 in array has the min value and index
 	for(unsigned int stride = 1; stride < blockDim.x*blockDim.y; stride*=2){
 		__syncthreads();
 		if(tid %(2*stride) == 0){
@@ -114,8 +136,13 @@ __global__ void two_opt(unsigned int *cycle, float *distance, unsigned int citie
 		}
 	}
 	
-	min_index_array[bid] = temp_min_index[0];
-	min_val_array[bid] = temp_min[0];
+	//save the min value for the block into global index for the block
+	if(tid == 0){
+		min_index_array[bid] = temp_min_index[0];	
+		min_val_array[bid] = temp_min[0];
+	}
+	
+	
 }
 
 void *tsp(void *arguments){
@@ -128,26 +155,32 @@ void *tsp(void *arguments){
 	unsigned int cities = args -> cities;
 	float *return_pointer = args -> return_pointer;
 	int device_index = args -> device_index;
+
+	//set the device to run on based on what was sent by master thread
 	CUDA_CALL(cudaSetDevice(device_index));
 
 	dim3 gridDim(BLOCKS_X, BLOCKS_Y);
 	dim3 blockDim(THREADS_X, THREADS_Y);
 	int min_index;
+
+	//stores the min value reported by each block
 	float *cpu_min_val = (float *)malloc(BLOCKS_X*BLOCKS_Y*sizeof(float));
 	float *gpu_min_val;
 	CUDA_CALL(cudaMalloc(&gpu_min_val, BLOCKS_X*BLOCKS_Y*sizeof(float)));
 
+	//stored the i and j values(as i*cities + j) of the swap that will result in the min value
 	unsigned int *cpu_min_index = (unsigned int *)malloc(BLOCKS_X*BLOCKS_Y*sizeof(unsigned int));
 	unsigned int *gpu_min_index;
 	CUDA_CALL(cudaMalloc(&gpu_min_index, BLOCKS_X*BLOCKS_Y*sizeof(unsigned int)));
 	
-
+	//stores current cycle
 	unsigned int cycle_size = (cities+1)*sizeof(unsigned int);
 	unsigned int *cpu_cycle = (unsigned int *)malloc(cycle_size);
 	unsigned int *global_optimal_cycle = (unsigned int *)malloc(cycle_size);
 	unsigned int *gpu_cycle;
 	CUDA_CALL(cudaMalloc(&gpu_cycle, cycle_size));
 
+	//run 8 streams
 	const int num_streams = 8;
   	cudaStream_t streams[num_streams];
   	cudaStream_t current_stream;
@@ -158,18 +191,26 @@ void *tsp(void *arguments){
 
 	float global_minima = FLT_MAX;
 	for(int i = device_index; i < cities; i = i + devices){
+
+		//allocate initial cities cycle
 		allocate_cycle(cpu_cycle, i, cities);
 		current_stream = streams[stream_index%num_streams];
 		while(true){
+			//get current cost
 			float temp_cost = get_total_cost(cpu_cycle, cpu_distance, cities);
+			//move current cycle to gpu and find best swap
 			CUDA_CALL(cudaMemcpyAsync(gpu_cycle, cpu_cycle, cycle_size, cudaMemcpyHostToDevice, current_stream));
 			two_opt<<<gridDim, blockDim, 0, current_stream>>>(gpu_cycle, gpu_distance, cities, gpu_min_val, gpu_min_index);
 
+			//move best reported swap and most decrease of that swap to CPU
 			CUDA_CALL(cudaMemcpyAsync(cpu_min_val, gpu_min_val, BLOCKS_X*BLOCKS_Y*sizeof(float), cudaMemcpyDeviceToHost,current_stream));
 			CUDA_CALL(cudaMemcpyAsync(cpu_min_index, gpu_min_index, BLOCKS_X*BLOCKS_Y*sizeof(int), cudaMemcpyDeviceToHost,current_stream));
 			cudaStreamSynchronize(current_stream);
 
+			//using this, calculated best values across blocks
 			min_index = get_min_val(cpu_min_val,BLOCKS_X*BLOCKS_Y);
+
+			//if the benefit of best swap is less than an increase of 0.1, we are close to minima, can exit
 			if(cpu_min_val[min_index] >= -.1){
 				if(global_minima > temp_cost){
 					global_minima = temp_cost;
@@ -178,6 +219,7 @@ void *tsp(void *arguments){
 				break;
 			}
 			else{
+				//otherwise find best indices for the swap and update the cycle of cities
 				int min_agg_index = cpu_min_index[min_index];
 				update_cycle(cpu_cycle, min_agg_index/cities, min_agg_index%cities);
 			}
